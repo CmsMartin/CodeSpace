a8384dcdec5daa88141db356b3f365f94629d20d
Cms <74807575+CmsMartin@users.noreply.github.com>
2022-06-05 06:59:43 +0000

    Add

---
+++ P5047.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e5 + 10;
+
+vector<tuple<int , int , int> > Vec1[MAXN] , Vec2[MAXN];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}
